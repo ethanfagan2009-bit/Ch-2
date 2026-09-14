0ebd2bf9a2f274d2ed70909060ce98993c3e78f0
ethanfagan2009-bit <ethanfagan2009@gmail.com>
2026-09-14 14:50:04 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,10 @@
+
+public class Date {
+	
+	public static void main(String[] arg){
+		String month= "September";
+		int day=14;
+		int year=2026;
+		System.out.print(month + " "+day+" " + year);
+	}
+}
---
+++ Time.java
@@ -0,0 +1,8 @@
+public class Time{
+	public static void main(String[] arg){
+	int hours=14;
+	int mins=30;
+	int seconds= hours*3600 + mins*60;
+	System.out.print(seconds);
+	}
+}
