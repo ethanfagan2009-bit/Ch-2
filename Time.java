220088c54159912478f62cbe4c873212d0033d78
ethanfagan2009-bit <ethanfagan2009@gmail.com>
2026-09-15 14:48:36 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -1,8 +1,21 @@
 public class Time{
 	public static void main(String[] arg){
 	int hours=14;
-	int mins=30;
+	int mins=28;
 	int seconds= hours*3600 + mins*60;
-	System.out.print(seconds);
+	double second=seconds;
+	int remaining= 24*60*60-seconds;
+	double percent = second/(24*60*60)*100;
+	System.out.println("seconds passed: " + seconds+"s");
+	System.out.println("seconds remaining: " + remaining+"s");
+	System.out.println("percent of day passed: "+percent+"%");
+	
+	hours = 14;
+	mins = 43;
+	seconds=hours*3600 + mins*60;
+	int timePassed= seconds-hours*3600;
+	System.out.println("time passed since class start: " + timePassed + "s");
+	
+	
 	}
 }
