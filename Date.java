266262bdd4b4f45a8815f22df8c34cf4bb7c8e20
ethanfagan2009-bit <ethanfagan2009@gmail.com>
2026-09-15 14:28:16 -0700

    Add files via upload

--- Date.java
+++ Date.java
@@ -1,10 +1,11 @@
-
 public class Date {
 	
 	public static void main(String[] arg){
 		String month= "September";
+		String weekday= "Monday";
 		int day=14;
 		int year=2026;
-		System.out.print(month + " "+day+" " + year);
+		System.out.println("American format:  " + weekday+", "+ day+" " +month + ", " + year);
+		System.out.println("European format:  " + weekday+" "+ month+" " +day + " " + year);
 	}
 }
